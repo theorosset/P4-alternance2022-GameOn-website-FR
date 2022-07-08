b91d7950ae6765147ae4267242f58819fece89e3
Rosset <theo.rosset@outlook.fr>
2022-07-08 14:26:00 +0200

    feat: verify form + (ci): change folder + (style) add new css

---
+++ starterOnly/js/formValidate.js
@@ -0,0 +1,145 @@
+const form = document.querySelector("form");
+
+//function main for validation
+function formValidation() {
+  //validation of email
+  form.email.addEventListener("input", (e) => {
+    emailValid(form.email);
+  });
+
+  //validation of firstName
+  form.first.addEventListener("input", (e) => {
+    firstNameValid();
+  });
+
+  //validation of lastName
+  form.last.addEventListener("input", (e) => {
+    lastNameValid();
+  });
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    checkBoxValid();
+    birthdateValid();
+    validQuantity();
+    if (
+      firstNameValid() === true &&
+      emailValid(form.email) === true &&
+      lastNameValid() === true &&
+      birthdateValid() === true &&
+      checkBoxAreChecked() === true
+    ) {
+      console.log("go");
+    } else {
+      return playAllFunctionVerif();
+    }
+  });
+}
+
+//-------------function for validation input form----------
+
+/**
+ *
+ * @param {HtmlInputElement} inputEmail
+ * @returns errors if email are not valid
+ */
+function emailValid(inputEmail) {
+  let emailRegExp = new RegExp(
+    "^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,15}$"
+  );
+  console.log(inputEmail);
+  let emailTest = emailRegExp.test(inputEmail.value);
+
+  /**
+   * if false error messsage for email are add
+   */
+  if (emailTest === false) {
+    return (document.querySelector("#errorEmail").innerText =
+      "Veuillez entrer un email valide");
+  }
+
+  //else nothing are add
+  else {
+    return (document.querySelector("#errorEmail").innerText = "");
+  }
+}
+
+//verify if one checkBox are checked
+function checkBoxAreChecked() {
+  const allBtnRadios = document.querySelectorAll(".btn-radio");
+  let checkBoxChecked = false;
+
+  allBtnRadios.forEach((checkBox) => {
+    if (checkBox.checked === true) {
+      checkBoxChecked = true;
+    }
+  });
+  return checkBoxChecked;
+}
+
+//checkBox show error if not check
+function checkBoxValid() {
+  if (checkBoxAreChecked() === false) {
+    return (document.querySelector("#errorCheckBox").innerText =
+      "Veuillez séletionner une option");
+  } else {
+    return (document.querySelector("#errorCheckBox").innerText = ""), true;
+  }
+}
+
+function validate(trueOrFalse) {
+  return trueOrFalse;
+}
+
+//show error if firstName has < 2
+function firstNameValid() {
+  if (form.first.value.length < 2) {
+    return (document.querySelector("#errorFirstName").innerText =
+      "Votre Prénom doit faire minimum 2 caratère");
+  } else {
+    return (document.querySelector("#errorFirstName").innerText = ""), true;
+  }
+}
+
+//show error if lastName has < 2
+function lastNameValid() {
+  if (form.last.value.length < 2) {
+    return (document.querySelector("#errorLastName").innerText =
+      "Votre Nom doit faire minimum 2 caratère");
+  } else {
+    return (document.querySelector("#errorLastName").innerText = ""), true;
+  }
+}
+// show error if birthdate are empty
+function birthdateValid() {
+  if (!form.birthdate.value.length) {
+    return (document.querySelector("#errorBirthday").innerText =
+      "Veuilliez entrez votre date de naissance");
+  } else {
+    return (document.querySelector("#errorBirthday").innerText = ""), true;
+  }
+}
+
+function validQuantity() {
+  console.log(parseInt(form.quantity.value));
+  if (
+    (parseInt(form.quantity.value) < 0 && parseInt(form.quantity.value) > 99) ||
+    !form.quantity.value
+  ) {
+    return (document.querySelector("#errorQuantity").innerText =
+      "Veuilliez entrer une valeur valide (entre 0 et 100)");
+  } else {
+    return (document.querySelector("#errorQuantity").innerText = "");
+  }
+}
+function playAllFunctionVerif() {
+  checkBoxValid();
+  birthdateValid();
+  validQuantity();
+  emailValid(form.email);
+  lastNameValid();
+  birthdateValid();
+  checkBoxAreChecked();
+}
+
+formValidation();
